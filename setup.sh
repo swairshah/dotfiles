11deb4ef73dc44469a6db1cb079724820adc0f04
Swair Shah <swairshah@gmail.com>
2019-12-28 16:01:59 -0800

    setup, move molokai

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ mkdir ~/.vim
 mkdir ~/.vim/colors
 cp dotfiles/wombat256.vim ~/.vim/colors
 cp dotfiles/lucius.vim ~/.vim/colors
+cp dotfiles/molokai.vim ~/.vim/colors
 mkdir ~/.vim/autolog
 
 wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -P ~/.vim/autoload
