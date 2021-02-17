37b4dd081fde1c8323dff9e2755b586e19ba20c1
swairshah <swairshah@gmail.com>
2021-02-17 11:45:16 -0800

    fix pyjlslime.vim error by placing it in scripts

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ ln -s dotfiles/vimrc .vimrc
 ln -s dotfiles/zshrc .zshrc
 mkdir ~/.vim
 mkdir ~/.vim/colors
+mkdir ~/.vim/scripts
+cp dotfiles/pyjlslime.vim ~/.vim/scripts
 cp dotfiles/wombat256.vim ~/.vim/colors
 cp dotfiles/lucius.vim ~/.vim/colors
 cp dotfiles/molokai.vim ~/.vim/colors
