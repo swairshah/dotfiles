d65dda14dd305acabb7922ec3c24fb64f58e792d
Swair Shah <swairshah@gmail.com>
2025-05-31 17:32:56 -0700

    add Go development plugins with debugging support

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -6,10 +6,47 @@ return {
     {'nvim-treesitter/playground'}, 
     {'tpope/vim-fugitive'},
     {'ziglang/zig.vim'},
+    -- Go development
+    {'ray-x/go.nvim',
+        dependencies = {
+            'ray-x/guihua.lua',
+            'neovim/nvim-lspconfig',
+            'nvim-treesitter/nvim-treesitter',
+        },
+        config = function()
+            require('go').setup()
+        end,
+        event = {"CmdlineEnter"},
+        ft = {"go", 'gomod'},
+        build = ':lua require("go.install").update_all_sync()'
+    },
+    -- Debugging support
+    {'mfussenegger/nvim-dap'},
+    {'leoluz/nvim-dap-go',
+        dependencies = {'mfussenegger/nvim-dap'},
+        config = function()
+            require('dap-go').setup()
+        end,
+        ft = "go"
+    },
+    {'rcarriga/nvim-dap-ui',
+        dependencies = {'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio'},
+        config = function()
+            require("dapui").setup()
+        end
+    },
     {'neovim/nvim-lspconfig'},
     {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/nvim-cmp'},
-    {'Exafunction/codeium.vim'},
+    {'hrsh7th/nvim-cmp',
+        dependencies = {
+            'hrsh7th/cmp-buffer',        -- Buffer completions
+            'hrsh7th/cmp-path',          -- Path completions
+            'hrsh7th/cmp-cmdline',       -- Command line completions
+            'saadparwaiz1/cmp_luasnip',  -- Snippet completions
+            'L3MON4D3/LuaSnip',         -- Snippet engine
+            'rafamadriz/friendly-snippets', -- Go snippets
+        }
+    },
     {'williamboman/mason.nvim'},
     {'williamboman/mason-lspconfig.nvim'},
     {'navarasu/onedark.nvim'},
