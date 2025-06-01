2718e3b2533e6869206441d89da893df324044b9
Swair Shah <swairshah@gmail.com>
2025-05-31 17:38:28 -0700

    add Go LSP config with keymaps and disable TypeScript server

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -33,7 +33,53 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
 -- These are just examples. Replace them with the language
 -- servers you have installed in your system
-require('lspconfig').gopls.setup({})
+require('lspconfig').gopls.setup({
+  on_attach = function(client, bufnr)
+    -- Enable format on save for Go files
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ async = false })
+        end,
+      })
+    end
+  end,
+})
+
+-- Go-specific settings
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.expandtab = false
+    
+    -- Go-specific keymaps
+    local opts = { buffer = true, silent = true }
+    vim.keymap.set('n', '<leader>gt', '<cmd>GoTest<cr>', opts)
+    vim.keymap.set('n', '<leader>gT', '<cmd>GoTestFunc<cr>', opts)
+    vim.keymap.set('n', '<leader>gr', '<cmd>GoRun<cr>', opts)
+    vim.keymap.set('n', '<leader>gs', '<cmd>GoStop<cr>', opts)
+    vim.keymap.set('n', '<leader>gb', '<cmd>GoBuild<cr>', opts)
+    vim.keymap.set('n', '<leader>gi', '<cmd>GoImport<cr>', opts)
+    vim.keymap.set('n', '<leader>gj', '<cmd>GoAddTag<cr>', opts)
+    vim.keymap.set('n', '<leader>gc', '<cmd>GoCoverage<cr>', opts)
+    vim.keymap.set('n', '<leader>gf', '<cmd>GoFillStruct<cr>', opts)
+    vim.keymap.set('n', '<leader>ge', '<cmd>GoIfErr<cr>', opts)
+  end,
+})
+
+-- DAP debugging keymaps
+vim.keymap.set('n', '<leader>db', '<cmd>lua require"dap".toggle_breakpoint()<cr>')
+vim.keymap.set('n', '<leader>dc', '<cmd>lua require"dap".continue()<cr>')
+vim.keymap.set('n', '<leader>di', '<cmd>lua require"dap".step_into()<cr>')
+vim.keymap.set('n', '<leader>do', '<cmd>lua require"dap".step_over()<cr>')
+vim.keymap.set('n', '<leader>dO', '<cmd>lua require"dap".step_out()<cr>')
+vim.keymap.set('n', '<leader>dr', '<cmd>lua require"dap".repl.toggle()<cr>')
+vim.keymap.set('n', '<leader>dl', '<cmd>lua require"dap".run_last()<cr>')
+vim.keymap.set('n', '<leader>du', '<cmd>lua require"dapui".toggle()<cr>')
+vim.keymap.set('n', '<leader>dt', '<cmd>lua require"dap".terminate()<cr>')
 require('lspconfig').rust_analyzer.setup({})
 -- require('lspconfig').pylsp.setup({})
 
@@ -63,20 +109,20 @@ require('lspconfig').zls.setup {
 }
 
 -- Setup TypeScript Language Server
-require("lspconfig").tsserver.setup {
-  on_attach = function(client, bufnr)
-    -- Key mappings (adjust as needed)
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    local opts = { noremap=true, silent=true }
-
-    -- Mappings.
-    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-  end,
-}
+-- require("lspconfig").tsserver.setup {
+--   on_attach = function(client, bufnr)
+--     -- Key mappings (adjust as needed)
+--     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+--     local opts = { noremap=true, silent=true }
+-- 
+--     -- Mappings.
+--     buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+--     buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+--     buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+--     buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+--     buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+-- 
+--     -- Enable completion triggered by <c-x><c-o>
+--     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+--   end,
+-- }
