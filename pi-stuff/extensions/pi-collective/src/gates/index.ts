ec1eadab7c454fd3bec5248d8b9b8a1cddfd2f7f
swairshah <swairshah@gmail.com>
2026-06-15 20:28:31 +0530

    remove pi-collective

--- pi-stuff/extensions/pi-collective/src/gates/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Gate checks for pi-collective
- */
-
-export { checkPublicRepo, type PublicRepoCheck } from "./public-repo.js";
-export { checkSelfContainedTests, type TestabilityCheck } from "./self-contained-tests.js";
-export { scanForPII, type PIIScanResult } from "./pii-scanner.js";
